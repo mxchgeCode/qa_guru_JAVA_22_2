a01145fb57e55db19817d4cac7f60f7f9cc471a2
mxchgeCode <101883708+mxchgeCode@users.noreply.github.com>
2023-09-07 00:46:27 +0200

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
+        open("https://www..com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
