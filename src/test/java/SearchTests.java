83f3a0cccb45ad8b7709170361c18cd6105880b4
mtx <m33wwe@gmail.com>
2023-09-07 01:27:47 +0300

    add hello msg

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+        System.out.println("Hello");
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
